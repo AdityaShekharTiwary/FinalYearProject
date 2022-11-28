17b17964f39681f606066326f48718563d4f999e
aditya.tiwary <aditya.tiwary@juspay.in>
2022-11-28 19:35:26 +0530

    fixed routes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import './style.css'
 const Navbar = () => {
     return(
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand ms-2" href="#home">Project Name</a>
+        <a class="navbar-brand ms-2" href="/home">Project Name</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
